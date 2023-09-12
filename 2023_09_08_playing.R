210e1c275a6813ba295edc60c70f207ac506bf0a
ThaoNguyenDA <thaong1412@gmail.com>
2023-09-11 21:53:28 -0700

    first comit

---
+++ 2023_09_08_playing.R
@@ -0,0 +1,10 @@
+hekko <- c("1","2")
+hello <- c(1, 2)
+3 > 4
+die <- 1:6
+names(die) <- c("one", "two", "three", "four", "five", "six")
+die
+hand1 <- c("ace", "king", "queen", "jack", "ten", "spades", "spades",
+           "spades", "spades", "spades")
+matrix(hand1, nrow = 5, byrow = TRUE)
+Sys.time()
